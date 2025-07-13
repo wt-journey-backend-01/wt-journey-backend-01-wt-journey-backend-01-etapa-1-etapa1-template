e13a9a2db7372a82b62ed0bc06689b0bdccaedd1
GustavoPR35 <gustavorodrigues9015@gmail.com>
2025-07-13 12:19:32 -0300

    estilização

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = 3000
 const path = require('path')
 const viewsPath = path.join(__dirname, 'views')
-const lanches = require('./public/data/lanches.js') 
+const lanches = require('./public/data/lanches.js')
 
 app.use(express.static(viewsPath))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -26,12 +26,22 @@ app.get('/sugestao', (req, res) => {
     res.status(200).send(`
         <head>
             <link rel="stylesheet" href="/css/style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
         </head>
         <body>
             <main>
-                <header>
-                    <a href="/">DevBurger</a>
-                </header>
+                <nav>
+                    <div class="logo-container">
+                        <img src="/image/devburger-logo.svg" alt="Logo da DevBurger" class="logo">
+                        <a href="/">DevBurger</a>
+                    </div>
+                    <div class="nav-links">
+                        <a href="/contato">CONTATO</a>
+                        <a href="/api/lanches">LANCHES</a>
+                    </div>
+                </nav>
                 <h1>Agradecemos pela sugestão de lanche!</h1>
                 <p>Nome: ${nome}</p>
                 <p>Ingredientes: ${ingredientes}</p>                
@@ -44,39 +54,30 @@ app.get('/api/lanches', (req, res) => {
     res.status(200).json(lanches)
 })
 
-// Opcional: uso do padrão PRG (POST, REDIRECT, GET)
 app.post('/contato', (req, res) => {
     const nome = req.body.nome
     const email = req.body.email
     const assunto = req.body.assunto
     const mensagem = req.body.mensagem
-
-    const params = new URLSearchParams({
-        nome,
-        email,
-        assunto,
-        mensagem
-    })
-
-    res.status(302).redirect(`/contato-recebido?${params.toString()}`)
-})
-
-// Opcional: uso do padrão PRG (POST, REDIRECT, GET)
-app.get('/contato-recebido', (req, res) => {
-    const nome = req.query.nome
-    const email = req.query.email
-    const assunto = req.query.assunto
-    const mensagem = req.query.mensagem
-
     res.status(200).send(`
         <head>
             <link rel="stylesheet" href="/css/style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
         </head>
         <body>
             <main>
-                <header>
-                    <a href="/">DevBurger</a>
-                </header>
+                <nav>
+                    <div class="logo-container">
+                        <img src="/image/devburger-logo.svg" alt="Logo da DevBurger" class="logo">
+                        <a href="/">DevBurger</a>
+                    </div>
+                    <div class="nav-links">
+                        <a href="/contato">CONTATO</a>
+                        <a href="/api/lanches">LANCHES</a>
+                    </div>
+                </nav>
                 <h1>Agradecemos pela mensagem!</h1>
                 <p>Nome: ${nome}</p>
                 <p>Email: ${email}</p>
@@ -87,7 +88,7 @@ app.get('/contato-recebido', (req, res) => {
     `)
 })
 
-// Opcional: trata rotas não existentes
+// Trata rotas não existentes
 app.use((req, res) => {
     res.status(404).sendFile(path.join(viewsPath, '404.html'))
 })
